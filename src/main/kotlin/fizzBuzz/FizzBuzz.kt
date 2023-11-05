16f0dbaef1299855e98a6d11cde95b52cd1ab686
stephane_lazarevic <stephane_lazarevic@connect-tech.sncf>
2023-11-05 19:39:20 +0100

    print Buzz if multiple of 5

--- src/main/kotlin/fizzBuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzBuzz/FizzBuzz.kt
@@ -6,6 +6,7 @@ class FizzBuzz {
         for (i in 1..100){
             when {
                 i % 3 == 0 -> println("Fizz")
+                i % 5 == 0 -> println("Buzz")
                 else -> println(i)
             }
         }
