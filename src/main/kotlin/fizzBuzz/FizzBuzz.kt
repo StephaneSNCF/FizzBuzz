f7e06bd27548e9dc494c66f633c40ac7119677c0
stephane_lazarevic <stephane_lazarevic@connect-tech.sncf>
2023-11-05 19:41:03 +0100

    print FizzBuzz if multiple of 3 and 5

--- src/main/kotlin/fizzBuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzBuzz/FizzBuzz.kt
@@ -5,6 +5,7 @@ class FizzBuzz {
     fun printNumber() {
         for (i in 1..100){
             when {
+                i % 3 == 0 && i % 5 == 0 -> println("FizzBuzz")
                 i % 3 == 0 -> println("Fizz")
                 i % 5 == 0 -> println("Buzz")
                 else -> println(i)
