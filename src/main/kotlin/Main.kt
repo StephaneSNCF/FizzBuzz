7ac823f025fe091949614cc5b51b6759c63b7121
stephane_lazarevic <stephane_lazarevic@connect-tech.sncf>
2023-11-05 19:44:23 +0100

    Add Main and refactor append method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import fizzBuzz.FizzBuzz
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(){
+
+    val fizzBuzz = FizzBuzz()
+    val res = fizzBuzz.printNumber()
+
+    println(res)
 }
